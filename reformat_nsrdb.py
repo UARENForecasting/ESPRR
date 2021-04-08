d95ddb0bba4450f73412ff4d5e2a6ac616848280
Antonio Lorenzo <atlorenzo@email.arizona.edu>
2021-04-08 07:39:20 -0700

    add elevation to files and fix scale factor attrs

--- reformat_nsrdb.py
+++ reformat_nsrdb.py
@@ -1,9 +1,43 @@
+from pathlib import Path
+
+
 import dask.array as da
 import h5py
+import numpy as np
+import pandas as pd
 import xarray as xr
 import zarr
 
+
 BOUNDARIES = ((31.0, 38.0), (-118.01, -103.01))
+
+
+def load_file(fname):
+    data = np.fromfile(fname, ">i2", 3601 * 3601).reshape((3601, 3601))
+    lat = int(fname.split("/")[-1][1:3])
+    lon = int(fname.split("/")[-1][4:7]) * -1
+    da = (
+        xr.DataArray(
+            data,
+            dims=["lat", "lon"],
+            coords=[np.linspace(lat + 1, lat, 3601), np.linspace(lon, lon + 1, 3601)],
+        )
+        .to_dataset(name="elevation")
+        .isel(lat=slice(None, None, 72), lon=slice(36, None, 72))
+    )
+    return da
+
+
+def load_elevation_ds():
+    srtm_dir = Path("/d4/uaren/srtm1/")
+    elevations = None
+    for f in srtm_dir.glob("*.hgt"):
+        da = load_file(str(f))
+        if elevations is None:
+            elevations = da
+        else:
+            elevations = elevations.combine_first(da)
+    return elevations
 
 
 def main():
@@ -31,10 +65,23 @@ def main():
     times.encoding = {"compressor": compressor}
 
     coordarr = da.from_array(irradiance_file["coordinates"])
-    lat = xr.DataArray(coordarr[limits, 0], dims=["spatial_idx"])
-    lon = xr.DataArray(coordarr[limits, 1], dims=["spatial_idx"])
+    lat = xr.DataArray(coordarr[limits, 0], dims=["spatial_idx"]).compute()
+    lon = xr.DataArray(coordarr[limits, 1], dims=["spatial_idx"]).compute()
     lat.encoding = {"compressor": compressor}
     lon.encoding = {"compressor": compressor}
+
+    # get elevations
+    elevation_ds = load_elevation_ds()
+    lat_lon_df = pd.DataFrame({"lat": lat, "lon": lon})
+    elev_df = lat_lon_df.apply(
+        lambda x: elevation_ds.elevation.sel(
+            lon=x.lon, lat=x.lat, method="nearest"
+        ).item(),
+        axis=1,
+    )
+    elev_df[elev_df < 0] = 0
+    elevation_da = xr.DataArray(elev_df.astype("uint16").values, dims=["spatial_idx"])
+    elevation_da.encoding = {"compressor": compressor}
 
     data = {}
     for var in ("ghi", "dni", "dhi", "fill_flag"):
@@ -49,18 +96,16 @@ def main():
     for var in ("air_temperature", "wind_speed"):
         vattr = dict(weather_file[var].attrs)
         scale_factor = vattr.pop("scale_factor")
-        data[var] = (
-            xr.DataArray(
-                da.from_array(weather_file[var])[:, limits],
-                dims=["time_idx", "spatial_idx"],
-                attrs={k: str(v) for k, v in vattr.items()},
-            )
-            .astype("float32")
-            .chunk((times.shape[0], 96))
-            / scale_factor
-        )
+        data[var] = xr.DataArray(
+            (da.from_array(weather_file[var])[:, limits]).astype("float32")
+            / scale_factor,
+            dims=["time_idx", "spatial_idx"],
+            attrs={k: str(v) for k, v in vattr.items()},
+        ).chunk((times.shape[0], 96))
         data[var].encoding = {"compressor": compressor, "scale_factor": scale_factor}
-    ds = xr.Dataset(data, coords={"lat": lat, "lon": lon, "times": times})
+    ds = xr.Dataset(
+        data, coords={"lat": lat, "lon": lon, "elevation": elevation_da, "times": times}
+    )
     ds.to_zarr(output_path, consolidated=True)
     weather_file.close()
     irradiance_file.close()
