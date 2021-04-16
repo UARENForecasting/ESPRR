c8b78d5080f786ddc8227c280094e864a6ba7ed6
Antonio Lorenzo <atlorenzo@email.arizona.edu>
2021-04-15 20:31:31 -0700

    redo stats for clr/cloudy

--- api/esprr_api/compute.py
+++ api/esprr_api/compute.py
@@ -1,5 +1,4 @@
 import calendar
-from functools import partial
 from uuid import UUID
 
 
@@ -11,7 +10,7 @@ from pvlib.location import Location  # type: ignore
 from pvlib.pvsystem import PVSystem  # type: ignore
 from pvlib.tracking import SingleAxisTracker  # type: ignore
 from pvlib.modelchain import ModelChain  # type: ignore
-from pvlib.solarposition import sun_rise_set_transit_spa, get_solarposition
+from pvlib.solarposition import get_solarposition
 
 
 from . import models, storage, utils
@@ -105,80 +104,55 @@ def compute_total_system_power(
     return out
 
 
-def _ramp_rate(period: int, series: pd.Series, quantile: float):
-
-    five_min_period = period // 5
-    # center? left?
+def _largest_ramps(period: int, series: pd.Series):
     out = (
-        series.diff(five_min_period)
+        series.resample(f"{period}min")
+        .mean()
+        .diff()
         .dropna()
+        .abs()
         .groupby(lambda x: x.month)
-        .quantile(quantile)
+        .quantile(0.95)
     )
-    # out *= 60 / period  # convert to hourly ramp rate
-    # ramp rates or absolute ramps?
     return out
 
 
-def _rise_set_ramps(system, ac_power):
-    system_center = system.boundary._rect.centroid
-    tz = "Etc/GMT+7"
-    index = pd.DatetimeIndex(np.unique(ac_power.index.tz_convert(tz).date)).tz_localize(
-        tz
-    )
-    rise_set_transit = sun_rise_set_transit_spa(index, system_center.y, system_center.x)
-
-    def func(key, group):
-        sign = 1 if key == "sunrise" else -1
-        ref_time = rise_set_transit[key].loc[pd.Timestamp(group.name, tz=tz)]
-        window = (ref_time, ref_time + sign * pd.Timedelta("2h"))
-        return group.loc[min(window) : max(window)].max() / 2
-
-    # find the max value w/in 2 hours of sunrise for every day,
-    # take mean over month
-    # should be using clearsky model?
-    sunrise_ramps = (
-        ac_power.tz_convert(tz)
+def _typical_ss_ramps(period: int, series: pd.Series):
+    out = (
+        series.resample(f"{period}min")
+        .mean()
+        .diff()
+        .dropna()
+        .abs()
         .groupby(lambda x: x.date)
-        .apply(partial(func, "sunrise"))
+        .quantile(0.95)
         .groupby(lambda x: x.month)
         .mean()
     )
-    sunset_ramps = (
-        ac_power.tz_convert(tz)
-        .groupby(lambda x: x.date)
-        .apply(partial(func, "sunset"))
-        .groupby(lambda x: x.month)
-        .mean()
-    )
-    return sunrise_ramps, sunset_ramps
+    return out
 
 
-def compute_statistics(system: models.PVSystem, ac_power: pd.Series) -> pd.DataFrame:
-    sunrise_ramps, sunset_ramps = _rise_set_ramps(system, ac_power)
+def compute_statistics(system: models.PVSystem, data: pd.DataFrame) -> pd.DataFrame:
     system_center = system.boundary._rect.centroid
-    zenith = get_solarposition(ac_power.index, system_center.y, system_center.x)[
-        "zenith"
-    ]
-    ac_power[zenith > 87] = np.nan  # for longer, does drop transitional periods
+    data = data.tz_convert("Etc/GMT+7")
+    zenith = get_solarposition(data.index, system_center.y, system_center.x)["zenith"]
+    data[zenith > 95] = np.nan
+    periods = (5, 10, 15, 30, 60)
     out = pd.DataFrame(
         {
-            "month": calendar.month_name[1:],
-            "sunrise": sunrise_ramps,
-            "sunset": sunset_ramps,
-            # or just take abs and only report 95%?
-            "5-min 95%": _ramp_rate(5, ac_power, 0.95),
-            "10-min 95%": _ramp_rate(10, ac_power, 0.95),
-            "15-min 95%": _ramp_rate(15, ac_power, 0.95),
-            "30-min 95%": _ramp_rate(30, ac_power, 0.95),
-            "60-min 95%": _ramp_rate(60, ac_power, 0.95),
-            "5-min 5%": _ramp_rate(5, ac_power, 0.05),
-            "10-min 5%": _ramp_rate(10, ac_power, 0.05),
-            "15-min 5%": _ramp_rate(15, ac_power, 0.05),
-            "30-min 5%": _ramp_rate(30, ac_power, 0.05),
-            "60-min 5%": _ramp_rate(60, ac_power, 0.05),
-        }
+            **{
+                ("all-sky", f"{p}-min 95%"): _largest_ramps(p, data.ac_power)
+                for p in periods
+            },
+            **{
+                ("sunrise/set", f"{p}-min typ."): _typical_ss_ramps(
+                    p, data.clearsky_ac_power
+                )
+                for p in periods
+            },
+        },
     )
+    out.index = pd.Index(calendar.month_name[1:], name="month")
     return out
 
 
