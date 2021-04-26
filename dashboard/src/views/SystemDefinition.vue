3a6847af360041175d154687199ac8d10ab7bcc8
Antonio Lorenzo <atlorenzo@email.arizona.edu>
2021-04-26 14:10:19 -0700

    remove number field arrows

--- dashboard/src/views/SystemDefinition.vue
+++ dashboard/src/views/SystemDefinition.vue
@@ -367,4 +367,16 @@ ul.error-list {
 }
 ul.error-list li {
 }
+/* remove number in/decrement */
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type="number"] {
+  -moz-appearance: textfield;
+}
 </style>
