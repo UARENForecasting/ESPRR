3dce5abe967536ec0c8b63a6cd1937438bf4ca8b
Will Holmgren <william.holmgren@gmail.com>
2021-10-29 12:57:58 -0700

    add fiona

--- api/setup.py
+++ api/setup.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
             "cryptography",
             "dask",
             "fastapi",
+            "fiona",
             "geopandas",
             "httpx",
             "pandas",
