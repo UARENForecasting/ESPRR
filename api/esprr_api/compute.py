e21d6163f111f4173232712472487250c50f7754
Patrick Bunn <ptwbunn@email.arizona.edu>
2022-04-15 15:40:27 -0700

    lintin

--- api/esprr_api/compute.py
+++ api/esprr_api/compute.py
@@ -109,7 +109,8 @@ def compute_total_system_power(
 
     # apply multiplier to ac_power
     out["ac_power"] = out["ac_power"].where(
-            clear, other=out["ac_power"] * multiplier.reindex(clear.index).ffill())
+        clear, other=out["ac_power"] * multiplier.reindex(clear.index).ffill()
+    )
 
     return out
 
@@ -122,20 +123,24 @@ def calculate_variable_multiplier(out):
     Multiplier is  not applied to low irradiance days
     """
     # find clear days
-    clear_days = (out["ac_power"].resample('1D').mean() >
-                  0.99 * out["clearsky_ac_power"].resample('1D').mean())
+    clear_days = (
+        out["ac_power"].resample("1D").mean()
+        > 0.99 * out["clearsky_ac_power"].resample("1D").mean()
+    )
     # find low irradiance days, predicited power is < 50% of clearsky power
-    low_irrad_days = (out["ac_power"].resample('1D').mean() <
-                      0.5 * out["clearsky_ac_power"].resample('1D').mean())
+    low_irrad_days = (
+        out["ac_power"].resample("1D").mean()
+        < 0.5 * out["clearsky_ac_power"].resample("1D").mean()
+    )
     # find daily std devs
     stds = (out["ac_power"]).groupby(out.index.date).std()
-    stds.index = pd.to_datetime(stds.index).tz_localize('utc')
+    stds.index = pd.to_datetime(stds.index).tz_localize("utc")
     # take std dev from non-clearsky days only for normalization
     stds = stds.where(~clear_days, other=np.nan)
     # compute a multiplication factor for non-clear times
     #         fixed_max  -  normalized daily standard deviation for non-clear
     #                       days gives variability for remainder of multiplier
-    m = 1.0 - 0.5*(stds - stds.min()) / (stds.max() - stds.min())
+    m = 1.0 - 0.5 * (stds - stds.min()) / (stds.max() - stds.min())
     # turn off mulitplier on low irradiance days
     m[low_irrad_days] = 1.0
     return m
